ef1844a7adee1271607f14a6ff6ae3941c8e6972
kukrik <tiit.papp@gmail.com>
2017-12-02 01:18:23 +0200

    Init

---
+++ install/project/includes/plugins/CounterUp.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * The CounterUp override file. This file gets installed into project/includes/plugins duing the initial installation
+ * of the plugin. After that, it is not touched. Feel free to modify this file as needed.
+ */
+
+
+namespace QCubed\Plugin;
+
+class CounterUp extends CounterUpBase {
+
+    /** Just try it out */
+    //protected $intDelay = '10';
+    //protected $intTime = '5000';
+
+}
